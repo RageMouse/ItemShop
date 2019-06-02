eb18b3decbfacb551721682e48e7829b84aa65c1
DSperber <dennissperber@live.nl>
2019-06-02 21:34:11 +0200

    Implemented MSSQL stored procedure for adding new Auctions.

--- DAL/MSSQL/AuctionMSSQLContext.cs
+++ DAL/MSSQL/AuctionMSSQLContext.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using DAL.Interface.DTOs;
 using DAL.Interface.Interfaces;
@@ -10,7 +12,30 @@ namespace DAL.MSSQL
     {
         public void AddAuction(AuctionDTO auction)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection con = Database.getConnection())
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("CreateAuction", con)
+                        { CommandType = CommandType.StoredProcedure })
+                    {
+                        cmd.Parameters.AddWithValue("@dateCreated", auction.DateCreated);
+                        cmd.Parameters.AddWithValue("@sold", auction.Sold);
+                        cmd.Parameters.AddWithValue("@minPrice", auction.MinPrice);
+                        cmd.Parameters.AddWithValue("@buyoutPrice", auction.BuyoutPrice);
+                        cmd.Parameters.AddWithValue("@itemId", auction.ItemId);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //todo a good exception
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public AuctionDTO GetById(int id)
